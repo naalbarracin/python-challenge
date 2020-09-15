37b83d219636fb170b562d5a15ef6100fb0ce328
Nadia Lashaw <nadia.lashaw@gmail.com>
2020-09-15 19:41:16 -0400

    final checking for pybank

--- pybank/main.py
+++ pybank/main.py
@@ -3,6 +3,7 @@ import csv
 
 #variable
 cvs_file_path=r"C:\Bootcamp\homework\python-challenge\python-challenge\pybank\resources\budget_data.csv"
+analisys_folder_file=r"C:\Bootcamp\homework\python-challenge\python-challenge\pybank\analysis\results.txt"
 total_number_of_months=0
 total=0
 day1_income={}
@@ -25,8 +26,9 @@ with open(cvs_file_path, newline='') as csvfile:
     print(csvreader)
 
     for row in csvreader:
+
         #print(row['Date'])
-        print(row)
+        #print(row)
         #to get total months
         total_number_of_months=total_number_of_months+1
         #total profit and loss
@@ -34,42 +36,38 @@ with open(cvs_file_path, newline='') as csvfile:
         #skip for first day because there is no previous day
         #sets the next day
         #print(counter)
-        if counter==0:
-            day1_income=row
-            counter=1
-            #print(day1_income)
-            #print("here")
-        #sets the previous day
+        if total_number_of_months==1:
+            day1_income = row
+            day2_income = row
         else:
-            day2_income=row
-            #print("me")
-            counter=0
-            #print("Total"+str((int(day2_income["Profit/Losses"])) - (int(day1_income["Profit/Losses"]))))
-            if max_change_income < (int(day2_income["Profit/Losses"])) - (int(day1_income["Profit/Losses"])):
-                max_change_income = (int(day2_income["Profit/Losses"])) - (int(day1_income["Profit/Losses"]))
+            day1_income=row
+            if max_change_income < (int(day1_income["Profit/Losses"])) - (int(day2_income["Profit/Losses"])):
+                max_change_income = (int(day1_income["Profit/Losses"])) - (int(day2_income["Profit/Losses"]))
                 day1_max=day1_income
                 day2_max=day2_income
-        print("111111")
-        print(day1_income)
-        print(day2_income) 
-        print("22222")
-        if total_number_of_months>1:
-            if max_decrease_income > (int(day2_income["Profit/Losses"])) - (int(day1_income["Profit/Losses"])):
-                max_decrease_income = (int(day2_income["Profit/Losses"])) - (int(day1_income["Profit/Losses"]))
+            if max_decrease_income > (int(day1_income["Profit/Losses"])) - (int(day2_income["Profit/Losses"])):
+                max_decrease_income = (int(day1_income["Profit/Losses"])) - (int(day2_income["Profit/Losses"]))
                 day1_max_dec=day1_income
                 day2_max_dec=day2_income
-                print("1111111111111111111111111")
-                print(max_decrease_income)
-                #break
-                #print(day1_income)
-                #print(day2_income) 
-           
-            
-        
-        
+            day2_income=row
+      
 
 print("total months: " + str(total_number_of_months))
 print("total: " + str(total))
 print("average change: " + str(total/total_number_of_months))
-print(day2_max["Date"] +" {" + str(max_change_income) + "}")
-print(day1_max_dec["Date"] +" {" + str(max_decrease_income) + "}")
+print("Greatest Increase in Profites: " + day1_max["Date"] +" {" + str(max_change_income) + "}")
+print("Greatest Decrease in Profites: " + day1_max_dec["Date"] +" {" + str(max_decrease_income) + "}")
+
+
+with open(analisys_folder_file, 'w') as filetowrite:
+    filetowrite.write("total months: " + str(total_number_of_months))
+    filetowrite.write('\n')
+    filetowrite.write("total: " + str(total))
+    filetowrite.write('\n')
+    filetowrite.write("average change: " + str(total/total_number_of_months))
+    filetowrite.write('\n')
+    filetowrite.write("Greatest Increase in Profites: " + day1_max["Date"] +" {" + str(max_change_income) + "}")
+    filetowrite.write('\n')
+    filetowrite.write("Greatest Decrease in Profites: " + day1_max_dec["Date"] +" {" + str(max_decrease_income) + "}")
+    filetowrite.write('\n')
+    filetowrite.write("Thank you for flying colombian airlines")
